5d8d0807e65585fadb732f236f88b0a6f3ed3e09
broCapang <U2000429@siswa.um.edu.my>
2023-11-16 14:55:07 +0900

    Tried to play with the main page picture size. Some wordings changed

--- src/modules/layout/components/footer-cta/index.tsx
+++ src/modules/layout/components/footer-cta/index.tsx
@@ -6,7 +6,7 @@ const FooterCTA = () => {
     <div className="bg-gray-500 w-full">
       <div className="content-container flex flex-col-reverse gap-y-8 small:flex-row small:items-center justify-between py-16 relative">
         <div>
-          <h3 className="text-2xl-semi">Shop the latest styles</h3>
+          <h3 className="text-2xl-semi">Shop the latest SUSU</h3>
           <div className="mt-6">
             <UnderlineLink href="/store">Explore SUFFY products</UnderlineLink>
           </div>
