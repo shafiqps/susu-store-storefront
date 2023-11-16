c61604b879598138a77be4bc581053ecd1ba3238
broCapang <U2000429@siswa.um.edu.my>
2023-11-16 14:39:40 +0900

    Try to play with the main page picture size

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -6,12 +6,12 @@ const Hero = () => {
     <div className="h-[90vh] w-full relative">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
         <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Introducing the Latest Summer Styles
+          Introducing SUFFY SUSU
         </h1>
         <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This season, our new summer collection embraces designs to provide
-          comfort and style - ensuring you&apos;re well-prepared for whatever
-          comes your way.
+          The world's first and only lactation milk that is made with 100%
+          natural ingredients and is 100% vegan.
+          Also available in a variety of flavors.
         </p>
         <UnderlineLink href="/store">Explore SUFFY products</UnderlineLink>
       </div>
--- src/modules/layout/components/footer-cta/index.tsx
+++ src/modules/layout/components/footer-cta/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const FooterCTA = () => {
   return (
-    <div className="bg-amber-100 w-full">
+    <div className="bg-gray-500 w-full">
       <div className="content-container flex flex-col-reverse gap-y-8 small:flex-row small:items-center justify-between py-16 relative">
         <div>
           <h3 className="text-2xl-semi">Shop the latest styles</h3>
@@ -12,9 +12,9 @@ const FooterCTA = () => {
           </div>
         </div>
 
-        <div className="relative w-full aspect-square small:w-[35%] small:aspect-[28/36]">
+        <div className="relative w-full aspect-square small:w-[50%] small:aspect-[36/36]">
           <Image
-            src="/cta_three.jpg"
+            src="/asian-female-fashion-store-owner-showing-thumb-up.webp"
             alt=""
             className="absolute inset-0"
             fill
